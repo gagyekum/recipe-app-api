22746e2c0d3feb82a207027017c886addb0e9902
Gideon Agyekum <gide2005@gmail.com>
2025-01-21 15:24:46 +0000

    Added management command to wait for db and run migrations

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,5 +1,24 @@
+import time
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
+
     def handle(self, *args, **options):
-        pass
+        self.stdout.write('Waiting for db...')
+
+        while True:
+            try:
+                self.check(databases=['default'])
+                break
+
+            except (Psycopg2Error, OperationalError):
+                self.stdout.write(
+                    'Database unavailable, waiting for 1 second...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database is available.'))
